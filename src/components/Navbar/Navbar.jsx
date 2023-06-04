2189a3c0f85c5706b3e1dba3f9ce2935d0414904
Alexis Campusano <ale.cmpb@gmail.com>
2023-06-04 16:19:58 -0400

    feat: add anchor link to home section in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,46 +8,48 @@ import './Navbar.scss';
 import DarkMode from '../DarkMode/DarkMode';
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
-  const navbarTitles = ['home', 'about', 'work', 'skills', 'blog', 'contact']
+    const [toggle, setToggle] = useState(false);
+    const navbarTitles = ['home', 'about', 'work', 'skills', 'blog', 'contact']
 
-  return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo' />
-      </div>
-    <ul className='app__navbar-links'>
-        {navbarTitles.map((item) => (
-          <li className='app__flex p-text' key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
-      <DarkMode />
-      <div className='app__navbar-menu'>
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {navbarTitles.map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
+    return (
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <a href="#home">
+                    <img src={images.logo} alt='logo' />
+                </a>
+            </div>
+            <ul className='app__navbar-links'>
+                {navbarTitles.map((item) => (
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
             </ul>
-          </motion.div>
-        )}
-      </div>
-    </nav>
-  );
+            <DarkMode />
+            <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {navbarTitles.map((item) => (
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
