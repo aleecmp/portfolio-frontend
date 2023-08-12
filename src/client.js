d7f9bdf1c785ac5fd0f19053e6b09fe5633c3c81
Alexis Campusano <ale.cmpb@gmail.com>
2023-08-11 21:10:03 -0400

    feat(migration): migrate project to Vite build tool

--- src/client.js
+++ src/client.js
@@ -1,12 +1,14 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+const REACT_APP_SANITY_PROJECT_ID = import.meta.env.VITE_REACT_APP_SANITY_PROJECT_ID;
+const REACT_APP_SANITY_TOKEN = import.meta.env.VITE_REACT_APP_SANITY_TOKEN;
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
